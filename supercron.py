90a0f63386c17311be6347478d068ca1c5c031f5
linostar <linux.anas@gmail.com>
2015-09-11 15:17:22 +0300

    Added FullHelpMessage class

--- supercron.py
+++ supercron.py
@@ -8,6 +8,9 @@ from crontab import CronTab
 class HelpMessage(Exception):
 	pass
 
+class FullHelpMessage(Exception):
+	pass
+
 class NotEnoughArguments(Exception):
 	pass
 
@@ -18,13 +21,19 @@ def parse_arguments():
 			raise HelpMessage
 		if len(sys.argv) == 2:
 			if (sys.argv[1] == "-h" or sys.argv[1] == "--help"):
-				raise HelpMessage
+				raise FullHelpMessage
 			else:
 				raise NotEnoughArguments
 		else:
 			return sys.argv[1], " ".join(sys.argv[2:])
 	except HelpMessage:
-		print("Syntax: supercron <script to be excecuted> <time or repetition clause>")
+		print("Usage: supercron <script to be excecuted> <time or repetition clause>")
+		print("Type 'supercron -h' or 'supercron --help' for detailed information and examples.")
+		sys.exit(0)
+	except FullHelpMessage:
+		print("SuperCron is utility that allows you to enter intelligent schedule commands that" +
+			"will be translated into crontab entries.")
+		print("Usage: supercron <script to be excecuted> <time or repetition clause>")
 		sys.exit(0)
 	except NotEnoughArguments:
 		print("Error: not enough arguments. Type 'supercon --help' for help.")
