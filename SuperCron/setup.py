d4bfd8e9b49a34f1e573847edc16a39a544f16cf
linostar <linux.anas@gmail.com>
2015-09-24 12:29:06 +0300

    Restructured to pypi package format

---
+++ SuperCron/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+	name = 'SuperCron',
+	packages = ['SuperCron'],
+	version = '0.1',
+	description = 'Intelligent interface to cron in UNIX systems',
+	author = 'Anas El Husseini',
+	author_email = 'linux.anas@gmail.com',
+	license = 'BSD',
+	url = 'https://github.com/linostar/SuperCron',
+	download_url = 'https://github.com/linostar/SuperCron/tarball/0.1',
+	keywords = ['cron', 'crontab', 'scheduling'],
+	install_requires = ['python-crontab>=1.9.3'],
+	classifiers = [
+	'Development Status :: 4 - Beta',
+	'Environment :: Console',
+	'Intended Audience :: End Users/Desktop',
+	'Intended Audience :: System Administrators',
+	'Topic :: Utilities',
+	'Topic :: System :: Systems Administration',
+	'License :: OSI Approved :: BSD License',
+	'Programming Language :: Python :: 2.7',
+	'Programming Language :: Python :: 3.3',
+	'Programming Language :: Python :: 3.4',
+	'Programming Language :: Python :: 3.5',
+	],
+)
