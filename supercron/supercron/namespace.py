6e3aa88c9d682cfd3b10d8789940aad5afe24e85
linostar <linux.anas@gmail.com>
2015-09-26 13:37:37 +0300

    Moved Namespace class to a separate module

---
+++ supercron/supercron/namespace.py
@@ -0,0 +1,24 @@
+class Namespace(dict):
+	"""A dict subclass that exposes its items as attributes"""
+
+	def __init__(self, obj={}):
+		super(Namespace, self).__init__(obj)
+
+	def __dir__(self):
+		return tuple(self)
+
+	def __repr__(self):
+		return "%s(%s)" % (type(self).__name__, super().__repr__())
+
+	def __getattribute__(self, name):
+		try:
+			return self[name]
+		except KeyError:
+			msg = "'%s' object has no attribute '%s'"
+			raise AttributeError(msg % (type(self).__name__, name))
+
+	def __setattr__(self, name, value):
+		self[name] = value
+
+	def __delattr__(self, name):
+		del self[name]
